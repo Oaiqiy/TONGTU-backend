5119f955dce3bdb15a189bd6d6346e9d3ff2ff62
pyiqiao <1320371940@qq.com>
2021-09-16 19:59:28 +0800

    Initial commit

---
+++ src/main/java/com/tongtu/tongtu/TongtuApplication.java
@@ -0,0 +1,13 @@
+package com.tongtu.tongtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongtuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tongtu/tongtu/TongtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongtu.tongtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
