7e66ec8bcc446552b29a07977dd3f1cf07997d78
pyiqiao <1320371940@qq.com>
2021-09-16 21:25:16 +0800

    create user class

---
+++ src/main/java/com/tongtu/tongtu/domain/User.java
@@ -0,0 +1,69 @@
+package com.tongtu.tongtu.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    /**
+     * valid email when user sign in
+     * user can user email to find back password
+     */
+    private String email;
+
+    /**
+     * user's storage
+     */
+    private Long maxStorage;
+    private Long usedStorage;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+}
