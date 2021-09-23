156366f6a55e4887ca355f820a54046d29eccee6
oaiqiy <1320371940@qq.com>
2021-09-23 21:28:06 +0800

    邮箱测试，已注释

--- src/main/java/com/tongtu/tongtu/init/UserInit.java
+++ src/main/java/com/tongtu/tongtu/init/UserInit.java
@@ -4,14 +4,20 @@ import java.util.Properties;
 
 import com.tongtu.tongtu.data.UserRepository;
 import com.tongtu.tongtu.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.mail.internet.MimeMessage;
+
+@Slf4j
 @Component
 public class UserInit {
     @Bean
@@ -31,38 +37,67 @@ public class UserInit {
         };
     }
 
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.yeah.net");
-        mailSender.setPort(587);
+//    @Bean
+//    public JavaMailSender javaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.yeah.net");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("tong_tu@yeah.net");
+//        mailSender.setPassword("QIRAYKDHVKVLDIZP");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 
-        mailSender.setUsername("tong_tu@yeah.net");
-        mailSender.setPassword("QIRAYKDHVKVLDIZP");
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-        
-        return mailSender;
-    }
-
-
-    @Bean
-    public CommandLineRunner sendEmaLineRunner(JavaMailSender javaMailSender) {
-        return new CommandLineRunner(){
-            @Override
-            public void run(String... args) throws Exception {
-                SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-                simpleMailMessage.setFrom("tong_tu@yeah.net");
-                simpleMailMessage.setTo("1320371940@qq.com");
-                simpleMailMessage.setSubject("test");
-                simpleMailMessage.setText("666");
-                javaMailSender.send(simpleMailMessage);
-
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner sendEmaLineRunner(JavaMailSender javaMailSender) {
+//        return new CommandLineRunner(){
+//            @Override
+//            public void run(String... args)  {
+//
+//                log.info("init mail");
+//
+//                SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//
+//                simpleMailMessage.setFrom("TongTu <tong_tu@outlook.com>");
+//                simpleMailMessage.setTo("1320371940@qq.com");
+//                simpleMailMessage.setSubject("通途验证码");
+//                simpleMailMessage.setText("<html>666666</html>");
+//                simpleMailMessage.setCc("tong_tu@outlook.com");
+//
+//                MimeMessage message = javaMailSender.createMimeMessage();
+//                MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+//
+//
+//
+//
+//
+//                try {
+//                    log.info("prepare");
+//                    messageHelper.setFrom("TongTu <tong_tu@outlook.com>");
+//                    messageHelper.setTo("1320371940@qq.com");
+//                    messageHelper.setSubject("通途验证码");
+//                    messageHelper.setText("<a href=\"https://blog.csdn.net/dayonglove2018/article/details/106784064\">老周的博客</a>",true);
+//                    messageHelper.setCc("tong_tu@outlook.com");
+//                    javaMailSender.send(messageHelper.getMimeMessage());
+//
+//                }catch (Exception e){
+//                    log.info("exception");
+//                    e.printStackTrace();
+//
+//                }
+//
+//
+//                log.info("send mail");
+//
+//            }
+//        };
+//    }
 }
