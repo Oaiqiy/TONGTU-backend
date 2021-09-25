dd5697e7605b267765993aaf8ebba163f13056da
oaiqiy <1320371940@qq.com>
2021-09-25 18:09:33 +0800

    sts

--- src/main/java/com/tongtu/tongtu/oss/OssConfig.java
+++ src/main/java/com/tongtu/tongtu/oss/OssConfig.java
@@ -3,6 +3,9 @@ package com.tongtu.tongtu.oss;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSClientBuilder;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.profile.DefaultProfile;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +23,11 @@ public class OssConfig {
     @Bean
     public OSS oss(){
         return new OSSClientBuilder().build(ossProperties.getEndpoint(),ossProperties.getAccessKeyId(),ossProperties.getAccessKeySecret());
+    }
+
+    @Bean
+    public IAcsClient iAcsClient(){
+        DefaultProfile profile = DefaultProfile.getProfile("cn-beijing",ossProperties.getAccessKeyId(),ossProperties.getAccessKeySecret());
+        return new DefaultAcsClient(profile);
     }
 }
--- src/test/java/com/tongtu/tongtu/oss/ConnectTest.java
+++ src/test/java/com/tongtu/tongtu/oss/ConnectTest.java
@@ -3,6 +3,12 @@ package com.tongtu.tongtu.oss;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.model.OSSObjectSummary;
 import com.aliyun.oss.model.ObjectListing;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.auth.sts.AssumeRoleRequest;
+import com.aliyuncs.auth.sts.AssumeRoleResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +17,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ConnectTest {
     @Autowired
     private OSS oss;
+
+    @Autowired
+    IAcsClient client;
 
     @Test
     public void connect(){
@@ -21,5 +30,25 @@ public class ConnectTest {
                     "(size = " + objectSummary.getSize() + ")");
         }
 
+    }
+
+    @Test
+    public void sts(){
+        AssumeRoleRequest request = new AssumeRoleRequest();
+
+        request.setRoleArn("acs:ram::1482221404522785:role/tongtu");
+        request.setRoleSessionName("tongtu");
+
+        //发起请求，并得到响应。
+        try {
+            AssumeRoleResponse response = client.getAcsResponse(request);
+            System.out.println(new Gson().toJson(response));
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            System.out.println("ErrCode:" + e.getErrCode());
+            System.out.println("ErrMsg:" + e.getErrMsg());
+            System.out.println("RequestId:" + e.getRequestId());
+        }
     }
 }
