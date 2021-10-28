b10a965fa0c02086a3f0114218cd0b8392ede5e1
oaiqiy <1320371940@qq.com>
2021-10-28 20:40:46 +0800

    feat: callback controller added function

--- src/main/java/com/tongtu/tongtu/api/oss/CallbackController.java
+++ src/main/java/com/tongtu/tongtu/api/oss/CallbackController.java
@@ -1,13 +1,13 @@
 package com.tongtu.tongtu.api.oss;
 
 import com.tongtu.tongtu.api.ResultInfo;
+import com.tongtu.tongtu.data.FileInfoRepository;
 import com.tongtu.tongtu.data.UserRepository;
 import com.tongtu.tongtu.domain.FileInfo;
 import com.tongtu.tongtu.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +17,7 @@ import java.util.Map;
 @AllArgsConstructor
 public class CallbackController {
     private UserRepository userRepository;
+    private FileInfoRepository fileInfoRepository;
 
     @PostMapping
     public ResultInfo<Map<String,String>> ossCallback(@RequestBody CallbackForm callbackForm){
@@ -24,11 +25,13 @@ public class CallbackController {
         Map<String,String> test= new HashMap<>();
 
 
-        User user= userRepository.findUserById(callbackForm.getId());
+        User user= userRepository.findUserById(callbackForm.getUser());
         user.uploadFile(callbackForm.getSize(), FileInfo.FileType.OTHER);
         userRepository.save(user);
-
+        fileInfoRepository.save(callbackForm.toFileInfo());
         test.put("usedStorage",user.getUsedStorage().toString());
+
+
         return new ResultInfo<>(0, "success", test);
     }
 }
