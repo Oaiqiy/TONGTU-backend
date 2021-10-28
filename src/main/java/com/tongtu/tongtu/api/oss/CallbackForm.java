c11c04d8dee662b8339cc9eceab02e3e439865ec
oaiqiy <1320371940@qq.com>
2021-10-28 20:31:45 +0800

    feat: get file type

--- src/main/java/com/tongtu/tongtu/api/oss/CallbackForm.java
+++ src/main/java/com/tongtu/tongtu/api/oss/CallbackForm.java
@@ -18,10 +18,43 @@ public class CallbackForm {
     private String object;
 
     public FileInfo toFileInfo(){
-        //TODO: get file type
 
-        return new FileInfo(bucket,object,size, FileInfo.FileType.OTHER,new User(user),new Device(device));
+        FileInfo.FileType fileType;
 
+        switch (mimeType.charAt(1)){
+            case 'm':
+                fileType = FileInfo.FileType.IMAGE;
+                break;
+            case 'i':
+                fileType = FileInfo.FileType.VIDEO;
+                break;
+            case 'e':
+                fileType = FileInfo.FileType.TEXT;
+                break;
+            case 'u':
+                fileType= FileInfo.FileType.AUDIO;
+                break;
+            case 'p':
+                fileType = applicationType();
+                break;
+            default:
+                fileType = FileInfo.FileType.OTHER;
+
+        }
+
+
+        return new FileInfo(bucket,object,size, fileType,new User(user),new Device(device));
+
+    }
+
+    private FileInfo.FileType applicationType(){
+        String type = mimeType.substring(11);
+        if(type.contains("ms"))
+            return FileInfo.FileType.TEXT;
+        else if(type.equals("pdf"))
+            return FileInfo.FileType.TEXT;
+        else
+            return FileInfo.FileType.OTHER;
     }
 
 
