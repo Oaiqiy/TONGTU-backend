f8047a133e8a17c14f41032dc6ecdbdc7b9c106e
oaiqiy <1320371940@qq.com>
2021-10-28 11:20:53 +0800

    test: device repository

--- src/test/java/com/tongtu/tongtu/data/RepositoryTest.java
+++ src/test/java/com/tongtu/tongtu/data/RepositoryTest.java
@@ -2,6 +2,7 @@ package com.tongtu.tongtu.data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tongtu.tongtu.api.ResultInfo;
+import com.tongtu.tongtu.domain.Device;
 import com.tongtu.tongtu.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class RepositoryTest{
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    FileInfoRepository fileInfoRepository;
+    @Autowired
+    DeviceRepository deviceRepository;
     @Test
     public void repoTest() throws Exception{
         User user = new User("123","324","324@fdsa.com");
-        userRepository.save(user);
+        User saved = userRepository.save(user);
 
         ObjectMapper objectMapper = new ObjectMapper();
 
         System.out.println(objectMapper.writeValueAsString(new ResultInfo<String>(0, "asdf", "fasfda")));
+
+        userRepository.delete(saved);
     }
+
+    @Test
+    public void deviceTest() throws Exception{
+        Device device = new Device();
+        device.setName("huawei p30");
+        device.setUser(new User(1L));
+        deviceRepository.save(device);
+    }
+
+
 }
