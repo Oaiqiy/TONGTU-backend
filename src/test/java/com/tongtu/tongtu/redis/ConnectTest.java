31157423f560a9c8a1e8df0913b1553b4aef9182
oaiqiy <1320371940@qq.com>
2021-11-19 11:02:56 +0800

    feat: redis

---
+++ src/test/java/com/tongtu/tongtu/redis/ConnectTest.java
@@ -0,0 +1,25 @@
+package com.tongtu.tongtu.redis;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class ConnectTest {
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @Test
+    public void connect(){
+
+
+       if (Boolean.TRUE.equals(redisTemplate.hasKey("tongtu"))){
+           System.out.println("has tongtu");
+       }else {
+           System.out.println("not has tongtu");
+       }
+
+    }
+}
