af2a7b7a997b1e0c9f1cf9c0ececb761a7d77130
oaiqiy <1320371940@qq.com>
2021-12-07 17:32:01 +0800

    feat: user register to create a bucket

--- src/main/java/com/tongtu/tongtu/api/user/MailController.java
+++ src/main/java/com/tongtu/tongtu/api/user/MailController.java
@@ -1,21 +1,24 @@
 package com.tongtu.tongtu.api.user;
 
+import com.aliyun.oss.OSS;
 import com.tongtu.tongtu.data.UserRepository;
 import com.tongtu.tongtu.domain.User;
 import com.tongtu.tongtu.security.jwt.TokenProcessor;
+import lombok.AllArgsConstructor;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping(value = "/user", name = "user")
+@AllArgsConstructor
 public class MailController {
     private final TokenProcessor tokenProcessor;
     private final UserRepository userRepository;
-    MailController(TokenProcessor tokenProcessor,UserRepository userRepository){
-        this.tokenProcessor=tokenProcessor;
-        this.userRepository=userRepository;
-    }
+    private final OSS oss;
 
 
     @GetMapping("/check")
@@ -29,9 +32,11 @@ public class MailController {
         if(username==null)
             return "验证失败";
 
+
         User user = userRepository.findUserByUsername(username);
         user.setVerified(true);
         userRepository.save(user);
+        oss.createBucket(DigestUtils.md5DigestAsHex(user.getUsername().getBytes(StandardCharsets.UTF_8)));
 
         return "验证成功";
     }
